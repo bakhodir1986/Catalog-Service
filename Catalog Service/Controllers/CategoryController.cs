283f35772b45678a49518eef1ab91d66dbb17afa
Bakhodir Radjabov <bakhodir.radjabov@gmail.com>
2022-10-25 20:18:21 +0500

    Web Service improvements added

--- Catalog Service/Controllers/CategoryController.cs
+++ Catalog Service/Controllers/CategoryController.cs
@@ -1,6 +1,42 @@
-﻿namespace Catalog_Service.Controllers
+﻿using Catalog_Business_Layer_Service;
+using Catalog_Persistence_Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog_Service.Controllers
 {
     public class CategoryController
     {
+        private readonly ICategoryManager categoryManager;
+
+        public CategoryController(ICategoryManager categoryManager)
+        {
+            this.categoryManager = categoryManager;
+        }
+
+        [HttpGet(Name = "GetAllCategory")]
+        public IEnumerable<Category> Get()
+        {
+            return categoryManager.GetCategories();
+        }
+
+        [HttpPost(Name = "AddCategory")]
+        [ValidateAntiForgeryToken]
+        public void Add(Category category)
+        {
+            categoryManager.AddCategory(category);
+        }
+
+        [HttpPut(Name = "UpdateCategory")]
+        [ValidateAntiForgeryToken]
+        public void Update(Category category)
+        {
+            categoryManager.UpdateCategory(category);
+        }
+
+        [HttpDelete(Name = "RemoveCategory")]
+        public void Remove(Category category)
+        {
+            categoryManager.DeleteCategory(category);
+        }
     }
 }
--- Catalog Service/Controllers/ProductController.cs
+++ Catalog Service/Controllers/ProductController.cs
@@ -1,6 +1,43 @@
-﻿namespace Catalog_Service.Controllers
+﻿using Catalog_Business_Layer_Service;
+using Catalog_Persistence_Service;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+
+namespace Catalog_Service.Controllers
 {
     public class ProductController
     {
+        private readonly IProductManager productManager;
+
+        public ProductController(IProductManager productManager)
+        {
+            this.productManager = productManager;
+        }
+
+        [HttpGet(Name = "GetAllProducts")]
+        public IEnumerable<Product> Get()
+        {
+            return productManager.GetProducts();
+        }
+
+        [HttpPost(Name = "AddCategory")]
+        [ValidateAntiForgeryToken]
+        public void Add(Product product)
+        {
+            productManager.AddProduct(product);
+        }
+
+        [HttpPut(Name = "UpdateCategory")]
+        [ValidateAntiForgeryToken]
+        public void Update(Product product)
+        {
+            productManager.UpdateProduct(product);
+        }
+
+        [HttpDelete(Name = "RemoveCategory")]
+        public void Remove(Product product)
+        {
+            productManager.DeleteProduct(product);
+        }
     }
 }
